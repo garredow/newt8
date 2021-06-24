f89ba09bf537b1046e9f968f4b41690ae1acac7f
Garrett Downs <garredow@outlook.com>
2021-06-24 18:35:49 -0400

    misc style improvements

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -23,6 +23,7 @@ import { ComponentBase } from '../models/ComponentBase';
 import { useState } from 'react';
 import { Page } from '../services/panels';
 import { moveArrayItem } from '../utilities/moveArrayItem';
+import { ButtonKind } from '../enums/buttonKind';
 
 export type SidebarProps = ComponentBase;
 
@@ -109,6 +110,7 @@ export function Sidebar(props: SidebarProps) {
                           >
                             <IconButton
                               size={32}
+                              kind={ButtonKind.Card}
                               type={ButtonType.Secondary}
                               className={styles.draggable}
                               icon={<MdCompareArrows />}
@@ -137,6 +139,7 @@ export function Sidebar(props: SidebarProps) {
 
                             <IconButton
                               size={32}
+                              kind={ButtonKind.Card}
                               type={ButtonType.Danger}
                               icon={<MdClose />}
                               title="Delete"
@@ -164,6 +167,7 @@ export function Sidebar(props: SidebarProps) {
         </DragDropContext>
         <IconButton
           size={40}
+          kind={ButtonKind.Card}
           type={ButtonType.Secondary}
           icon={<MdAdd />}
           title="Add Page"
@@ -178,6 +182,7 @@ export function Sidebar(props: SidebarProps) {
         />
         <IconButton
           size={40}
+          kind={ButtonKind.Card}
           type={ButtonType.Secondary}
           icon={<MdEdit />}
           title="Edit Pages"
@@ -188,6 +193,7 @@ export function Sidebar(props: SidebarProps) {
         <Link to="/themer">
           <IconButton
             size={48}
+            kind={ButtonKind.Card}
             type={ButtonType.Primary}
             icon={<MdColorLens />}
             title="Themes"
@@ -197,6 +203,7 @@ export function Sidebar(props: SidebarProps) {
         <Link to="/settings">
           <IconButton
             size={48}
+            kind={ButtonKind.Card}
             type={ButtonType.Primary}
             icon={<MdSettings />}
             title="Themes"
