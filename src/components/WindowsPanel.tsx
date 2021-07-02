3dc4c7176ee045112106baed2421431f65d52048
Garrett Downs <garredow@outlook.com>
2021-07-02 10:38:58 -0400

    enable picking specific window in window panel

--- src/components/WindowsPanel.tsx
+++ src/components/WindowsPanel.tsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react';
+import { ButtonKind } from '../enums/buttonKind';
+import { ButtonType } from '../enums/buttonType';
 import { PanelType } from '../enums/panelType';
 import { Window } from '../models/Browser';
 import { ComponentBase } from '../models/ComponentBase';
 import { DraggablePanelProps } from '../models/DraggablePanelProps';
 import { getWindows, switchToTab } from '../services/browser';
 import { getPanelConfig, PanelOptions } from '../services/panels';
+import { Button } from '../ui-components/button/Button';
 import { Card, CardHeader } from '../ui-components/card';
 import { SiteRow } from '../ui-components/card/SiteRow';
 import { Panel, PanelContent } from '../ui-components/panel';
+import styles from './WindowsPanel.module.css';
 
-type WindowsPanelOptions = PanelOptions;
+type WindowsPanelOptions = PanelOptions & {
+  showCardTitles: boolean;
+  windowId: number;
+};
 
 type WindowsPanelProps = ComponentBase &
   DraggablePanelProps & {
@@ -20,6 +27,7 @@ type WindowsPanelProps = ComponentBase &
 
 export function WindowsPanel(props: WindowsPanelProps) {
   const [windows, setWindows] = useState<Window[]>([]);
+  const [showWindowPicker, setShowWindowPicker] = useState(false);
 
   const options: WindowsPanelOptions = Object.assign(
     getPanelConfig(PanelType.Windows).defaultOptions,
@@ -30,6 +38,21 @@ export function WindowsPanel(props: WindowsPanelProps) {
     getWindows().then((res) => setWindows(res));
   }, []);
 
+  useEffect(() => {
+    if (options.windowId === undefined) {
+      setShowWindowPicker(true);
+    }
+  }, [options.windowId]);
+
+  function handleChooseWindow(windowId: number) {
+    const newOpts: WindowsPanelOptions = {
+      ...options,
+      windowId,
+    };
+    props.onOptionsChanged(newOpts);
+    setShowWindowPicker(false);
+  }
+
   return (
     <Panel
       panelId={props.panelId}
@@ -38,23 +61,74 @@ export function WindowsPanel(props: WindowsPanelProps) {
       onOptionsChanged={props.onOptionsChanged as any}
       onDeletePanel={props.onDeletePanel}
       data-testid={props['data-testid']}
+      extraSettings={
+        <Button
+          text="Choose New Window"
+          kind={ButtonKind.Panel}
+          fullWidth
+          onClick={() => setShowWindowPicker(true)}
+        />
+      }
     >
-      <PanelContent columns={options.columns}>
-        {windows.map((window) => (
-          <Card key={window.id}>
-            <CardHeader text={`Window ${window.id}`} />
-            {window.tabs.map((tab) => (
-              <SiteRow
-                key={tab.id}
-                title={tab.title}
-                iconUrl={`chrome://favicon/size/32@1x/${tab.url}`}
-                url={tab.url}
-                onClick={() => switchToTab(tab.windowId, tab.id)}
-              />
-            ))}
+      {showWindowPicker ? (
+        <PanelContent columns={1}>
+          <Card>
+            <CardHeader text="Choose a window" />
+            <div className={styles.windowPickerContainer}>
+              <div>
+                <Button
+                  text="All Windows"
+                  kind={ButtonKind.Card}
+                  type={ButtonType.Primary}
+                  fullWidth
+                  onClick={() => handleChooseWindow(0)}
+                />
+                <p>Each window will be its own card.</p>
+              </div>
+              {windows.map((window) => (
+                <div key={window.id}>
+                  <Button
+                    text={`Window ${window.id}`}
+                    kind={ButtonKind.Card}
+                    type={ButtonType.Secondary}
+                    fullWidth
+                    onClick={() => handleChooseWindow(window.id!)}
+                  />
+                  {window.tabs.slice(0, 5).map((tab) => (
+                    <SiteRow
+                      key={tab.id}
+                      title={tab.title}
+                      iconUrl={`chrome://favicon/size/32@1x/${tab.url}`}
+                    />
+                  ))}
+                </div>
+              ))}
+            </div>
           </Card>
-        ))}
-      </PanelContent>
+        </PanelContent>
+      ) : (
+        <PanelContent columns={options.columns}>
+          {windows
+            .filter(
+              (window) =>
+                options.windowId === window.id || options.windowId === 0
+            )
+            .map((window) => (
+              <Card key={window.id}>
+                <CardHeader text={`Window ${window.id}`} />
+                {window.tabs.map((tab) => (
+                  <SiteRow
+                    key={tab.id}
+                    title={tab.title}
+                    iconUrl={`chrome://favicon/size/32@1x/${tab.url}`}
+                    url={tab.url}
+                    onClick={() => switchToTab(tab.windowId, tab.id)}
+                  />
+                ))}
+              </Card>
+            ))}
+        </PanelContent>
+      )}
     </Panel>
   );
 }
