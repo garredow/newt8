b86546a47b7d915d7b44356af9dabfc70b78ae82
Garrett Downs <garredow@outlook.com>
2021-07-25 07:47:36 -0400

    cleanup

--- src/ui-components/panel/PanelHeader.test.tsx
+++ src/ui-components/panel/PanelHeader.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { PanelHeader } from '.';
 
 describe('PanelHeader', () => {
@@ -17,66 +17,6 @@ describe('PanelHeader', () => {
     );
 
     expect(getByText(props.text)).toBeVisible();
-  });
-
-  test('sets title as editable', () => {
-    const props = {
-      text: 'text',
-      editable: true,
-      onTitleChanged: jest.fn(),
-    };
-
-    const { getByText } = render(
-      <PanelHeader {...props}>
-        <div>child text</div>
-      </PanelHeader>
-    );
-
-    expect(getByText(props.text)).toHaveAttribute('contentEditable', 'true');
-  });
-
-  test('edits a title', () => {
-    const props = {
-      text: 'text',
-      editable: true,
-      onTitleChanged: jest.fn(),
-    };
-
-    const { getByText } = render(
-      <PanelHeader {...props}>
-        <div>child text</div>
-      </PanelHeader>
-    );
-
-    const heading = getByText(props.text);
-    heading.focus();
-    fireEvent.keyDown(heading, { key: 'Enter' });
-
-    expect(props.onTitleChanged.mock.calls.length).toEqual(1);
-  });
-
-  test('only submits new title on enter', () => {
-    const props = {
-      text: 'text',
-      editable: true,
-      onTitleChanged: jest.fn(),
-    };
-
-    const { getByText } = render(
-      <PanelHeader {...props}>
-        <div>child text</div>
-      </PanelHeader>
-    );
-
-    const heading = getByText(props.text);
-    heading.focus();
-    fireEvent.keyDown(heading, { key: 't' });
-    fireEvent.keyDown(heading, { key: 'e' });
-    fireEvent.keyDown(heading, { key: 's' });
-    fireEvent.keyDown(heading, { key: 't' });
-    fireEvent.keyDown(heading, { key: 'Enter' });
-
-    expect(props.onTitleChanged.mock.calls.length).toEqual(1);
   });
 
   test('renders children', () => {
--- src/ui-components/panel/PanelHeader.tsx
+++ src/ui-components/panel/PanelHeader.tsx
@@ -1,40 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { ComponentBase } from '../../models/ComponentBase';
 import { mixin } from '../../utilities/mixin';
 import styles from './PanelHeader.module.css';
 
 type PanelHeaderProps = ComponentBase & {
   text: string;
-  editable?: boolean;
-  onTitleChanged: (title: string) => void;
 };
 
 export function PanelHeader(props: PanelHeaderProps) {
-  const [changed, setChanged] = useState(false);
-
   return (
     <div
-      className={mixin(styles.root, props.className!)}
+      className={mixin(styles.root, props.className)}
       data-testid={props['data-testid']}
     >
-      <h1
-        className={styles.title}
-        contentEditable={props.editable}
-        suppressContentEditableWarning
-        onKeyDown={(ev) => {
-          if (ev.key !== 'Enter' && ev.key !== 'Escape') {
-            setChanged(true);
-            return;
-          }
-
-          ev.preventDefault();
-          setChanged(false);
-          props.onTitleChanged((ev.target as HTMLHeadingElement).innerText);
-        }}
-      >
-        {props.text}
-      </h1>
-      {changed ? <span className={styles.message}>Enter to save</span> : null}
+      <h1 className={styles.title}>{props.text}</h1>
       <div className={styles.flex} />
       {props.children}
     </div>
